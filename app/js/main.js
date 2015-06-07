cf2f3a3f32a92cc035e7beac9e1daf91c1214237
Kelley Rose <krose422@gmail.com>
2015-06-07 18:50:54 -0400

    Fixed responsive so min width is 310px

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,6 @@ var Todo = function (options) {
 
 var taskList = [];
 
-// on submit event action
 $('#addTask').on('submit', function (event) {
   event.preventDefault();
   addTask();
@@ -24,13 +23,10 @@ $('#addTask').on('submit', function (event) {
 
 function addTask () {
   var task = $('#task').val();
-  // if task name is not blank, add instance and push to taskList array
   if (task !== '') {
     var taskInstance = new Todo({ task: task });
     taskList.push(taskInstance);
-    // append taskList items to HTML
     $('#tasks').prepend(template.addtask(taskInstance));
-    // run count function to update counter in HTML
     count();
   }
 }
@@ -41,33 +37,23 @@ function addTask () {
 
 $('#tasks').on('click', 'li', function (event) {
   event.preventDefault();
-  // add 'complete' class to add checkmark in HTML
   $(this).addClass('complete');
-  // get item text from HTML and find object in array that matches
   var tTask = $(this).find('span').text();
   var taskToEdit = _.find(taskList, { task: tTask });
-  // change found item status to 'Done'
   taskToEdit.status = 'Done';
-  // timestamp when completed into HTML
   var date = moment().format('MMM DD, YYYY');
   var time = moment().format('hh:mm');
   $(this).find('.timestamp').html('completed on ' + date + ' at ' + time);
-  // run count function to update counter in HTML
   count();
 });
 
 $('#tasks').on('click', '.complete', function (event) {
   event.preventDefault();
-  // remove class from HTML item to remove checkmark
   $(this).removeClass('complete');
-  // remove timestamp from HTML
   $(this).find('.timestamp').html('');
-  // get item text from HTML and find object in array that matches
   var tTask = $(this).find('span').text();
   var taskToEdit = _.find(taskList, { task: tTask });
-  // change found item status to 'Open'
   taskToEdit.status = 'Open';
-  // run count function to update counter in HTML
   count();
 });
 
@@ -77,15 +63,14 @@ $('#tasks').on('click', '.complete', function (event) {
 
 $('#clear').on('click', function (event) {
   event.preventDefault();
-  // update array by removing all items with 'Done' status
+
   taskList = _.reject(taskList, function (y) {
     return y.status === 'Done';
   });
-  // update count with array length since only 'Open' tasks remain
+
   $('.count').html(taskList.length);
-  // empty current list from HTML
+
   $('#tasks').empty();
-  // append each item in updated task list back into HTML ('Open' only)
   _.each(taskList, function (data) {
     $('#tasks').append(template.addtask(data));
   });
@@ -94,7 +79,6 @@ $('#clear').on('click', function (event) {
 // SORT ITEMS WITH BUTTONS IN HTML
 // ================================
 
-// Declare new arrays needed
 var incompleteList = [];
 var completeList = [];
 
@@ -104,11 +88,11 @@ $('.sorting').on('click', '#incomplete', function (event) {
   event.preventDefault();
   $(this).addClass('sorted');
   $(this).siblings().removeClass('sorted');
-  // populate array with only completed items
+
   incompleteList = _.filter(taskList, function (t) {
     return t.status === 'Open';
   });
-  // empty previous array from HTML and append new array
+
   $('#tasks').empty();
   _.each(incompleteList, function (data) {
     $('#tasks').append(template.addtask(data));
@@ -122,17 +106,18 @@ $('.sorting').on('click', '#completed', function (event) {
   event.preventDefault();
   $(this).addClass('sorted');
   $(this).siblings().removeClass('sorted');
-  // populate array with only completed items
+
   completeList = _.filter(taskList, function (t) {
     return t.status === 'Done';
   });
-  // empty previous array from HTML and append new array
+
   $('#tasks').empty();
   _.each(completeList, function (data) {
     $('#tasks').append(template.addtask(data));
   });
-  // add 'complete' class back on items since they are all completed
+
   $('li').addClass('complete');
+  console.log(taskList);
 });
 
 // SORT: ALL ITEMS
@@ -141,14 +126,15 @@ $('.sorting').on('click', '#all', function (event) {
   event.preventDefault();
   $(this).addClass('sorted');
   $(this).siblings().removeClass('sorted');
-  // empty previous array from HTML
+
   $('#tasks').empty();
   // add class back on to completed items in initial array for checkmark
+  // KELLEY TO FIX BUG - ADDING CLASS BACK ON TO COMPLETED TASKS
   _.each(taskList, function (data) {
+
     if (data.status === 'Done') {
       $('li').addClass('complete');
     }
-  // append all items back into HTML
   $('#tasks').append(template.addtask(data));
   });
 });
